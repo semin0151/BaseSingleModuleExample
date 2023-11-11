cda33ea7f455fc141b938595b1bd68f2fc0df15a
osemin <semin015123@gmail.com>
2023-11-11 12:34:52 +0900

    add(BaseActivity) : add startActivity

--- app/src/main/java/com/semin/basesinglemoduleexample/presentation/base/activity/BaseActivity.kt
+++ app/src/main/java/com/semin/basesinglemoduleexample/presentation/base/activity/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.semin.basesinglemoduleexample.presentation.base.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,12 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun startActivity(activity: Class<out BaseActivity>, init: ((Intent) -> Unit)? = null) {
+        val intent = Intent(this, activity).apply {
+            init?.invoke(this)
+        }
+        startActivity(intent)
     }
 }
