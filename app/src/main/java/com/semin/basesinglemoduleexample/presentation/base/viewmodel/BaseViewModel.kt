188b9b05736dad9871583a8e18bc4b769cba48a5
osemin <semin015123@gmail.com>
2023-11-11 12:36:13 +0900

    fix(BaseViewModel) : add initState

--- app/src/main/java/com/semin/basesinglemoduleexample/presentation/base/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/semin/basesinglemoduleexample/presentation/base/viewmodel/BaseViewModel.kt
@@ -1,13 +1,19 @@
 package com.semin.basesinglemoduleexample.presentation.base.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.semin.basesinglemoduleexample.presentation.util.extension.channelToStateFlow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.StateFlow
 
-abstract class BaseViewModel<State, Event>: ViewModel() {
+abstract class BaseViewModel<Event, State>: ViewModel() {
     protected abstract var currentEvent: Event
     protected val events = Channel<Event>()
     abstract val state: StateFlow<State>
+
+    fun initState(initialState: State): StateFlow<State> {
+        return events.channelToStateFlow(initialState, ::changeState, viewModelScope)
+    }
 
     protected suspend fun onEvent(event: Event) {
         currentEvent = event
